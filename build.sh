e7efce58287645bd3ec9970a83b658ed78c6ea8a
Peter Polačko <peter.polacko@gmail.com>
2016-10-05 22:38:52 +0200

    travis integration

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ dotnet build ./src/Asn1Net.Test --configuration Release
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test ./test/Asn1Net.Test -c Release -f netcoreapp1.0
+dotnet test ./test/Asn1Net.Test
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
